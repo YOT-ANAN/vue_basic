8fe7e3e4ea4a0f4d963cbebec3dbbd46eabb1590
YOT-ANAN <32262282+YOT-ANAN@users.noreply.github.com>
2022-01-03 21:48:25 +0700

    learn about attribute binding

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <section>
+    <img v-bind:src="picture" v-bind:width="size" :height="size" />
+    <!-- สามารถเขียน v-bind โดยใช้ : แทน -->
     <h1>ชื่อ-นามสกุล : {{ getFullName() }}</h1>
     <h1>อายุ : {{ age }} ปี</h1>
     <p>ที่อยู่ : <span v-html="address"></span></p>
+    <a :href="social" target="_blank">facebook</a>
   </section>
 </template>
 
@@ -15,6 +18,9 @@ export default {
       lastname: "Tioasakun",
       age: 23,
       address: "<strong>หาดใหญ่</strong>",
+      picture: "https://cdn-icons-png.flaticon.com/512/5815/5815726.png",
+      size: 200,
+      social : "https://www.facebook.com/t.yotanan"
     };
   },
   methods: {
